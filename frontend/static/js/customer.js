fc93b6ba96c360da7b480051547de539a9e0f68a
cooluk92 <cooluk92@gmail.com>
2024-02-01 08:30:06 +0900

    [Update] 고객 DB 헤더 필터 추가

--- frontend/static/js/customer.js
+++ frontend/static/js/customer.js
@@ -48,10 +48,66 @@ function handleKeyDown(e, textarea, id) {
 }
 
 $(document).ready(function() {
+    $('#customerTable thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#customerTable thead');
+
     var table = $('#customerTable').DataTable({
         dom : 'Blfrtip',
         lengthChange : true,
-        order : [[ 0, "desc" ]],
+        order : [[ 1, "asc" ],  [2, "asc"]],
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function () {
+            var api = this.api();
+
+            // For each column
+            api
+                .columns()
+                .eq(0)
+                .each(function (colIdx) {
+                    // Set the header cell to contain the input element
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+
+                    // On every keypress in this input
+                    $(
+                        'input',
+                        $('.filters th').eq($(api.column(colIdx).header()).index())
+                    )
+                        .off('keyup change')
+                        .on('change', function (e) {
+                            // Get the search value
+                            $(this).attr('title', $(this).val());
+                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+
+                            var cursorPosition = this.selectionStart;
+                            // Search the column for that value
+                            api
+                                .column(colIdx)
+                                .search(
+                                    this.value != ''
+                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                        : '',
+                                    this.value != '',
+                                    this.value == ''
+                                )
+                                .draw();
+                        })
+                        .on('keyup', function (e) {
+                            e.stopPropagation();
+
+                            $(this).trigger('change');
+                            $(this)
+                                .focus()[0]
+                                .setSelectionRange(cursorPosition, cursorPosition);
+                        });
+                });
+        },
         buttons: [
             {
                 text: '추가',
