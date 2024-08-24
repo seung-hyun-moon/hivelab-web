87add8fabc564926e423687fbbfafad2e19f8cad
cooluk92 <cooluk92@gmail.com>
2024-08-24 15:14:07 +0900

    [Update] 연락처 속도 개선
    - Server-side processing

--- backend/routers/contact.py
+++ backend/routers/contact.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from typing import Any, Dict
 
 import pandas as pd
-from fastapi import APIRouter, Depends, UploadFile, HTTPException
+from fastapi import APIRouter, Depends, UploadFile, HTTPException, Request
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from backend.schemas.contact import Contact, ContactCreate, ContactUpdate
@@ -21,6 +23,46 @@ class ContactRouter(BaseCRUD):
 
     def update_item(self, item_id: int, item: ContactUpdate, db: Session = Depends(get_db)):
         return super().update_item(item_id=item_id, item=item, db=db)
+
+    # Server Side Processing
+    def get_items(self, request: Request, db: Session = Depends(get_db)) -> JSONResponse:
+        draw = int(request.query_params.get('draw', 1))
+        start = int(request.query_params.get('start', 0))
+        length = int(request.query_params.get('length', 10))
+        search_value = request.query_params.get('search[value]', '')
+        order_column_index = int(request.query_params.get('order[0][column]', 0))
+        order_dir = request.query_params.get('order[0][dir]', 'asc')
+
+        query = db.query(self.model)
+
+        # 검색 처리
+        if search_value:
+            query = query.filter(self.model.name.ilike(f"%{search_value}%"))
+
+        # 정렬 처리
+        if order_dir == 'asc':
+            query = query.order_by(getattr(self.model, self.model.__table__.columns.keys()[order_column_index]).asc())
+        else:
+            query = query.order_by(getattr(self.model, self.model.__table__.columns.keys()[order_column_index]).desc())
+
+        total_records = query.count()
+
+        # 페이징 처리
+        query = query.offset(start).limit(length)
+
+        items = query.all()
+
+        # Pydantic 스키마로 직렬화
+        data = [self.get_schema.from_orm(item).dict() for item in items]
+
+        response = {
+            "draw": draw,
+            "recordsTotal": total_records,
+            "recordsFiltered": total_records,
+            "data": data
+        }
+
+        return JSONResponse(content=response)
 
     def upload_excel_to_db(self, file: UploadFile, db: Session = Depends(get_db)):
         print(file.filename)
--- frontend/static/js/contact.js
+++ frontend/static/js/contact.js
@@ -11,11 +11,6 @@ function formatDate() {
 
 $(document).ready(function() {
     $('#loading-icon').show();
-    $('#contactTable thead tr')
-        .clone(true)
-        .addClass('filters')
-        .appendTo('#contactTable thead').hide();
-
     var table = $('#contactTable').DataTable({
         dom : 'Blfrtip',
         lengthChange : true,
@@ -23,6 +18,7 @@ $(document).ready(function() {
         orderCellsTop: true,
         fixedHeader: true,
         "pageLength": 25,
+        serverSide: true,
         buttons: [
             {
                 text: '추가',
@@ -39,53 +35,6 @@ $(document).ready(function() {
 //            'copy', 'excel'
         ],
         initComplete: function() {
-            $('#contactTable thead .filters').show();
-            var api = this.api();
-
-            // For each column
-            api
-                .columns()
-                .eq(0)
-                .each(function (colIdx) {
-                    // Set the header cell to contain the input element
-                    var cell = $('.filters th').eq(
-                        $(api.column(colIdx).header()).index()
-                    );
-                    var title = $(cell).text();
-                    $(cell).html('<input type="text" placeholder="' + title + '" />');
-
-                    // On every keypress in this input
-                    $(
-                        'input',
-                        $('.filters th').eq($(api.column(colIdx).header()).index())
-                    )
-                        .off('keyup change')
-                        .on('change', function (e) {
-                            // Get the search value
-                            $(this).attr('title', $(this).val());
-                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
-                            var cursorPosition = this.selectionStart;
-                            // Search the column for that value
-                            api
-                                .column(colIdx)
-                                .search(
-                                    this.value != ''
-                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                        : '',
-                                    this.value != '',
-                                    this.value == ''
-                                )
-                                .draw();
-                        })
-                        .on('keyup', function (e) {
-                            e.stopPropagation();
-
-                            $(this).trigger('change');
-                            $(this)
-                                .focus()[0]
-                                .setSelectionRange(cursorPosition, cursorPosition);
-                        });
-                });
             $('#loading-icon').hide();
             console.log('loading-icon');
         },
@@ -107,7 +56,22 @@ $(document).ready(function() {
 
         ajax: {
             url: '/api/contact/',
-            dataSrc: ''
+            type: 'GET',
+            data: function(d) {
+                return $.extend({}, d, {
+                    // 추가로 필요한 파라미터를 여기에 추가 가능
+                });
+            },
+            dataSrc: function(json) {
+                $('#loading-icon').hide();
+                return json.data; // 서버에서 전달한 데이터의 위치
+            },
+            beforeSend: function() {
+                $('#loading-icon').show();
+            },
+            complete: function() {
+                $('#loading-icon').hide();
+            }
         },
         columns: [
             { data: 'name' },
